2b9f2ea870ba9fdc145e2a5c0c040b16c2b3bcce
Fadil <fadilfdl2710@gmail.com>
2025-01-23 15:47:21 +0700

    save

--- lib/app/modules/modules_profile/profile_edit_name/views/profile_edit_name_view.dart
+++ lib/app/modules/modules_profile/profile_edit_name/views/profile_edit_name_view.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/get_instance.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
-import 'package:mmimobile/app/modules/modules_auth/data/controller/user_controller.dart';
 import 'package:mmimobile/app/styles/color.dart';
 import 'package:mmimobile/app/styles/fonts.dart';
 import 'package:mmimobile/app/styles/shadow.dart';
-import 'package:mmimobile/app/widget/form_apps_two_widget.dart';
+import 'package:get/get_instance/get_instance.dart';
 import '../controllers/profile_edit_name_controller.dart';
+import 'package:mmimobile/app/widget/form_apps_two_widget.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:mmimobile/app/modules/modules_auth/data/controller/user_controller.dart';
 
 class ProfileEditNameView extends GetView<ProfileEditNameController> {
   const ProfileEditNameView({super.key});
