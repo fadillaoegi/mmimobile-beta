ccb6d4936a34f6e27f8e3ccfd11f8ab38f3011dc
fadillaoegi <fadilfdl2710@gmail.com>
2025-03-03 02:28:32 +0700

    save

--- lib/app/modules/modules_auth/sign_in/views/sign_in_view.dart
+++ lib/app/modules/modules_auth/sign_in/views/sign_in_view.dart
@@ -21,6 +21,7 @@ class SignInView extends GetView<SignInController> {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<SignInController>();
+    // final sizeScreen = MediaQuery.sizeOf(context);
     return Obx(
       () => ModalProgressHUD(
         inAsyncCall: controller.isLoading.value,
@@ -38,127 +39,134 @@ class SignInView extends GetView<SignInController> {
                   ),
                   child: IntrinsicHeight(
                     child: CanvaApps(
-                      bg: AssetConfigFLdev.bgLoginNew,
-                      widget: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: constraints.maxHeight / 2.8,
-                          ),
-                          Text.rich(
-                            textAlign: TextAlign.start,
-                            TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: "Selamat datang di MMI Apps\n",
-                                  // text: "Masuk\n",
-                                  style: secondary700.copyWith(fontSize: 20.0),
-                                ),
-                                TextSpan(
-                                  text: "Masuk untuk melanjutkan",
-                                  style: secondary400.copyWith(fontSize: 16.0),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20.0,
-                          ),
-                          Form(
-                            key: controller.formKey,
-                            child: Column(
+                        bg: AssetConfigFLdev.bgLoginNew,
+                        widget: Stack(
+                          alignment: Alignment.bottomCenter,
+                          children: [
+                            Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                IntlPhoneField(
-                                  controller: controller.phoneController.value,
-                                  validator: (p0) {
-                                    if (p0!.toString().isEmpty || p0 == "") {
-                                      return "Phone is required ";
-                                    }
-                                    return null;
-                                  },
-                                  decoration: InputDecoration(
-                                    labelText: 'Phone Number',
-                                    focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                            color: ColorApps.primary)),
-                                    border: OutlineInputBorder(
-                                      borderSide: BorderSide(),
-                                    ),
-                                  ),
-                                  initialCountryCode: 'ID',
-                                  onChanged: (phone) {
-                                    print(phone.completeNumber);
-                                  },
-                                ),
-                                const SizedBox(
-                                  height: 10.0,
-                                ),
-                                Obx(
-                                  () => FormAppsTwo(
-                                    controller: controller.passController.value,
-                                    labelText: "Password",
-                                    obscure: controller.isObsecure.value,
-                                    suffixIcon: true,
-                                    validator: (p0) {
-                                      if (p0!.isEmpty || p0 == "") {
-                                        return "Password is required ";
-                                      } else if (p0.length < 8) {
-                                        return "Password must be 8 characters ";
-                                      }
-                                      return null;
-                                    },
-                                    onTap: () => controller.hideSeePass(),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 6.0,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    TextButton(
-                                        onPressed: () {
-                                          Get.toNamed(Routes.forgotPassword);
-                                        },
-                                        child: Text(
-                                          "Lupa Password",
-                                          style: secondary600.copyWith(
-                                              fontSize: 13.0),
-                                        ))
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 6.0,
-                                ),
-                                BtnApps(
-                                  onPress: () => controller.signIn(context),
-                                  text: "Masuk",
-                                ),
                                 SizedBox(
-                                  height: 60,
+                                  height: constraints.maxHeight / 2.8,
                                 ),
-
-                                // NOTE: UNCOMMENT IF YOU NEED THIS FEATURE
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    TextButton(
-                                      onPressed: () {},
-                                      child: Text(
-                                        "www.mashmoshem.co.id",
-                                        style:
-                                            disable500.copyWith(fontSize: 14.0),
+                                Text.rich(
+                                  textAlign: TextAlign.start,
+                                  TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: "Selamat datang di MMI Apps\n",
+                                        // text: "Masuk\n",
+                                        style: secondary700.copyWith(
+                                            fontSize: 20.0),
                                       ),
-                                    ),
-                                  ],
+                                      TextSpan(
+                                        text: "Masuk untuk melanjutkan",
+                                        style: secondary400.copyWith(
+                                            fontSize: 16.0),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 20.0,
+                                ),
+                                Form(
+                                  key: controller.formKey,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      IntlPhoneField(
+                                        controller:
+                                            controller.phoneController.value,
+                                        validator: (p0) {
+                                          if (p0!.toString().isEmpty ||
+                                              p0 == "") {
+                                            return "Phone is required ";
+                                          }
+                                          return null;
+                                        },
+                                        decoration: InputDecoration(
+                                          labelText: 'Phone Number',
+                                          focusedBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: ColorApps.primary)),
+                                          border: OutlineInputBorder(
+                                            borderSide: BorderSide(),
+                                          ),
+                                        ),
+                                        initialCountryCode: 'ID',
+                                        onChanged: (phone) {
+                                          print(phone.completeNumber);
+                                        },
+                                      ),
+                                      const SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Obx(
+                                        () => FormAppsTwo(
+                                          controller:
+                                              controller.passController.value,
+                                          labelText: "Password",
+                                          obscure: controller.isObsecure.value,
+                                          suffixIcon: true,
+                                          validator: (p0) {
+                                            if (p0!.isEmpty || p0 == "") {
+                                              return "Password is required ";
+                                            } else if (p0.length < 8) {
+                                              return "Password must be 8 characters ";
+                                            }
+                                            return null;
+                                          },
+                                          onTap: () => controller.hideSeePass(),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 6.0,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Get.toNamed(
+                                                    Routes.forgotPassword);
+                                              },
+                                              child: Text(
+                                                "Lupa Password",
+                                                style: secondary600.copyWith(
+                                                    fontSize: 13.0),
+                                              ))
+                                        ],
+                                      ),
+                                      const SizedBox(
+                                        height: 6.0,
+                                      ),
+                                      BtnApps(
+                                        onPress: () =>
+                                            controller.signIn(context),
+                                        text: "Masuk",
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "www.mashmoshem.co.id",
+                                    style: disable500.copyWith(fontSize: 14.0),
+                                  ),
                                 ),
                               ],
                             ),
-                          )
-                        ],
-                      ),
-                    ),
+                          ],
+                        )),
                   ),
                 ),
               ),
