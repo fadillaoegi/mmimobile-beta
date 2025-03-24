cd57fba6852a6b4c0bf404d016d003f398c03a57
Fadil <fadilfdl2710@gmail.com>
2025-03-24 13:02:45 +0700

    save

--- lib/app/api/api.dart
+++ lib/app/api/api.dart
@@ -3,7 +3,7 @@ class ApiApps {
   // static const root = "https://mmi.entis.app/api";
 
   // NOTE: ROOT LOCAL
-  static const root = "http://192.168.1.33/EntIS-MMI/api";
+  static const root = "http://190.110.0.69/EntIS-MMI/api";
 
   // NOTE: CUSTOMER AUTH API
   static const signIn = "$root/signin";
--- lib/app/modules/modules_profile/profile/views/profile_view.dart
+++ lib/app/modules/modules_profile/profile/views/profile_view.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/get_instance.dart';
-import 'package:get/get_navigation/get_navigation.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
-import 'package:mmimobile/app/helper/helper_fldev.dart';
-import 'package:mmimobile/app/utils/image_converter_fldev.dart';
-import 'package:mmimobile/app/widget/image_circle_widget.dart';
 import '../controllers/profile_controller.dart';
 import 'package:mmimobile/app/styles/color.dart';
 import 'package:mmimobile/app/styles/fonts.dart';
 import 'package:mmimobile/app/styles/shadow.dart';
+import 'package:get/get_instance/get_instance.dart';
 import 'package:mmimobile/app/routes/app_pages.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:mmimobile/app/helper/helper_fldev.dart';
 import 'package:mmimobile/app/configs/asset_config.dart';
-import 'package:mmimobile/app/widget/item_list_widget.dart';
 import 'package:mmimobile/app/session/user_session.dart';
 import 'package:mmimobile/app/configs/format_config.dart';
+import 'package:mmimobile/app/widget/item_list_widget.dart';
 import 'package:mmimobile/app/widget/canva_apps_widget.dart';
+import 'package:mmimobile/app/widget/image_circle_widget.dart';
+import 'package:mmimobile/app/utils/image_converter_fldev.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:mmimobile/app/widget/alert/alert_dialog_widget.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:mmimobile/app/modules/modules_auth/data/controller/user_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
