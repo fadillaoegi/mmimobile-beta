540410a9c631ddfcb95df7b0e0c7ebc05fe2269e
Fadil <fadilfdl2710@gmail.com>
2025-04-07 17:59:55 +0700

    save

--- lib/app/helpers/refresh_data_fldev.dart
+++ lib/app/helpers/refresh_data_fldev.dart
@@ -1,10 +1,36 @@
 import 'package:dio/dio.dart';
+import 'package:get/get_rx/get_rx.dart';
 import 'package:mmimobile/app/api/api.dart';
-import 'package:mmimobile/app/api/request_apps.dart';
 import 'package:mmimobile/app/data/models/user_model.dart';
+import 'package:mmimobile/app/data/sources/source_apps.dart';
+import 'package:mmimobile/app/session/user_session.dart';
 
 class RefreshDataFldev {
-  static Future<List<User>?> refreshUser(FormData formData) async {
-    final result = await RequestApp.postFutureDio(ApiApps.getUser, formData);
+  // NOTE: REFRESH DATA USER
+  static Future<void> refreshDataUser(FormData formData) async {
+    final isLoading = false.obs;
+    try {
+      isLoading(true);
+      // Ambil data terbaru dari session (bisa dari API)
+      final result = await SourceApps.hitApiToMap(ApiApps.getUser, formData);
+      User user = User.fromJson(result!['data']);
+      SessionUserFLdev.saveUser(user);
+      // User? updatedUser = await SessionUserFLdev.getUser();
+    } catch (e) {
+      print("Error refreshing user data: $e");
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  // NOTE: REFRESH DATA MEMBERSHIP
+  static Future<void> refreshDataMembership(FormData formData, bool loading) async {
+    final isLoading = false.obs;
+    try {
+      isLoading(true);
+      // Ambil data terbaru dari session (bisa dari API)
+    } catch (e) {
+      print("Error refreshing membership data");
+    }
   }
 }
--- lib/app/modules/modules_profile/profile/controllers/profile_controller.dart
+++ lib/app/modules/modules_profile/profile/controllers/profile_controller.dart
@@ -3,10 +3,11 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_rx/get_rx.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
-import 'package:mmimobile/app/api/api.dart';
-import 'package:mmimobile/app/data/models/user_model.dart';
-import 'package:mmimobile/app/data/sources/source_apps.dart';
-import 'package:mmimobile/app/session/user_session.dart';
+// import 'package:mmimobile/app/api/api.dart';
+// import 'package:mmimobile/app/data/models/user_model.dart';
+// import 'package:mmimobile/app/data/sources/source_apps.dart';
+// import 'package:mmimobile/app/session/user_session.dart';
+import 'package:mmimobile/app/helpers/refresh_data_fldev.dart';
 import 'package:mmimobile/app/modules/modules_auth/data/controller/user_controller.dart';
 
 class ProfileController extends GetxController {
@@ -28,67 +29,29 @@ class ProfileController extends GetxController {
     final formData = FormData.fromMap({
       'customer_id': userData.user.customerId,
     });
-    try {
-      // Ambil data terbaru dari session (bisa dari API)
-      final result = await SourceApps.hitApiToMap(ApiApps.getUser, formData);
-      User user = User.fromJson(result!['data']);
-      SessionUserFLdev.saveUser(user);
-      // User? updatedUser = await SessionUserFLdev.getUser();
 
-      // if (updatedUser != null) {
-      //   final userController = Get.put(UserController());
-      //   userController.setData(updatedUser);
-      // }
-    } catch (e) {
-      print("Error refreshing user data: $e");
-    } finally {
-      isLoading(false);
-    }
+    RefreshDataFldev.refreshDataUser(formData);
+
+    isLoading(false);
+    // isLoading(true);
+    // final formData = FormData.fromMap({
+    //   'customer_id': userData.user.customerId,
+    // });
+    // try {
+    //   // Ambil data terbaru dari session (bisa dari API)
+    //   final result = await SourceApps.hitApiToMap(ApiApps.getUser, formData);
+    //   User user = User.fromJson(result!['data']);
+    //   SessionUserFLdev.saveUser(user);
+    //   // User? updatedUser = await SessionUserFLdev.getUser();
+
+    //   // if (updatedUser != null) {
+    //   //   final userController = Get.put(UserController());
+    //   //   userController.setData(updatedUser);
+    //   // }
+    // } catch (e) {
+    //   print("Error refreshing user data: $e");
+    // } finally {
+    //   isLoading(false);
+    // }
   }
-
-// NOTE: FUNCTION
-  // emailForgot(BuildContext context) {
-  //   final email = emailController.value.text;
-
-  //   if (formKey.currentState!.validate()) {
-  //     if (email != _emailStatic.value) {
-  //       Get.dialog(
-  //         AlertDialogNoAction(
-  //           title: "Your email is not registered",
-  //           content: email,
-  //           lotties: AssetConfigFLdev.lottieFailed,
-  //         ),
-  //         barrierDismissible: false,
-  //       );
-
-  //       Future.delayed(
-  //         Duration(seconds: 3),
-  //         () => Get.back(),
-  //       );
-
-  //     } else {
-  //       Get.dialog(
-  //         const AlertDialogNoAction(
-  //           title: "code sent to your email",
-  //           lotties: AssetConfigFLdev.lottieSuccess,
-  //         ),
-  //         barrierDismissible: false,
-  //       );
-
-  //       Timer(
-  //         const Duration(seconds: 3),
-  //         () {
-  //           Navigator.pop(context);
-  //           Timer(
-  //             const Duration(seconds: 1),
-  //             () {
-  //               Get.toNamed(Routes.otp, arguments: email);
-  //             },
-  //           );
-  //         },
-  //       );
-  //     }
-  //   }
-  //   emailController.value.clear();
-  // }
 }
